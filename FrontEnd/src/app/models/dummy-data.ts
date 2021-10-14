3f9f0f3b4cfa8885964e012eb5bf958b26ba5338
sriharik998 <sriharik998@gmail.com>
2021-10-14 07:17:27 +0530

    project uploaded

---
+++ FrontEnd/src/app/models/dummy-data.ts
@@ -0,0 +1,82 @@
+import { Task } from "./Task";
+
+export const DummyTasks : Task[] = [
+    {
+        taskID:1001,
+        taskHeading:"Task 01",
+        taskContent:"Lorem ipsum dolor sit amet consectetur",
+        dueDate:"2021/10/07",
+        priorityLevel:"HIGH",
+        category:"demos",
+        completed:false        
+        
+      },
+      {
+        taskID:1002,
+        taskHeading:"Task 02",
+        taskContent:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus non repellendus maxime tempora.",
+        dueDate:"2021/10/05",
+        priorityLevel:"MEDIUM",
+        category:"samples",
+        completed:false      
+      },
+      {
+        taskID:1003,
+        taskHeading:"Task 03",
+        taskContent:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus non repellendus maxime tempora.Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus non repellendus maxime tempora.",
+        dueDate:"2021/10/12",
+        priorityLevel:"LOW",
+        category:"samples",
+        completed:true      
+      },
+      {
+        taskID:1004,
+        taskHeading:"Task 04",
+        taskContent:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus non repellendus maxime tempora.",
+        dueDate:"2021/10/07",
+        priorityLevel:"MEDIUM",
+        category:"demos",
+        completed:false      
+      },
+      {
+        taskID:1005,
+        taskHeading:"Task 05",
+        taskContent:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus non repellendus maxime tempora.",
+        dueDate:"2021/10/02",
+        priorityLevel:"HIGH",
+        category:"demos",
+        completed:true      
+      },
+      {
+        taskID:1006,
+        taskHeading:"Task 06",
+        taskContent:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus non repellendus maxime tempora.",
+        dueDate:"2021/10/03",
+        priorityLevel:"MEDIUM",
+        category:"demos",
+        completed:false      },
+      {
+        taskID:1007,
+        taskHeading:"Task 07",
+        taskContent:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus non repellendus maxime tempora.",
+        dueDate:"2021/10/09",
+        priorityLevel:"LOW",
+        category:"samples",
+        completed:false      },
+      {
+        taskID:1008,
+        taskHeading:"Task 08",
+        taskContent:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus non repellendus maxime tempora.",
+        dueDate:"2021/10/11",
+        priorityLevel:"HIGH",
+        category:"samples",
+        completed:false      },
+      {
+        taskID:1009,
+        taskHeading:"Task 09",
+        taskContent:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus non repellendus maxime tempora.",
+        dueDate:"2021/10/04",
+        priorityLevel:"MEDIUM",
+        category:"demos",
+        completed:false      }
+    ]
